39000cd494262b19a98b163335c668f6a11ace65
luojia65 <me@luojia.cc>
2019-03-16 23:13:27 +0800

    Update

--- heart/light.c
+++ heart/light.c
@@ -11,17 +11,42 @@
 
 unsigned char img[W * H * 3];
 
+typedef struct { float sd, emissive; } Result;
+
 float circleSDF(float x, float y, float cx, float cy, float r) {
     float ux = x-cx, uy=y-cy;
     return sqrtf(ux*ux+uy*uy)-r;
 }
 
+Result unionOp(Result a, Result b) {
+    return (a.sd<b.sd)?a:b;
+}
+
+Result intersectOp(Result a, Result b) {
+    Result r = (a.sd>b.sd)?b:a;
+    r.sd = (a.sd>b.sd)?a.sd:b.sd;
+    return r;
+}
+
+Result subtractOp(Result a, Result b) {
+    Result r = a;
+    r.sd = (a.sd > -b.sd)?a.sd: -b.sd;
+    return r;
+}
+
+Result scene(float x, float y) {
+    Result r1 = { circleSDF(x, y, 0.5f, 0.5f, 0.25f), 2.0f };
+    Result r2 = { circleSDF(x, y, 0.3f, 0.7f, 0.05f), 1.8f };
+    Result r3 = { circleSDF(x, y, 0.7f, 0.3f, 0.1f), 0.0f };
+    return unionOp(subtractOp(r1, r2), r3);
+}
+
 float trace(float ox, float oy, float dx, float dy) {
     float t=0.0f ;
     for(int i=0;i<MAX_STEP&&t<MAX_DISTANCE; i++) {
-        float sd = circleSDF(ox+dx*t, oy+dy*t, 0.5f, 0.5f, 0.2f);
-        if(sd < EPSILON) return 2.0f;
-        t+=sd;
+        Result r = scene(ox+dx*t, oy+dy*t);
+        if(r.sd < EPSILON) return r.emissive;
+        t+=r.sd;
     }
     return 0.0f;
 }
@@ -29,8 +54,6 @@ float trace(float ox, float oy, float dx, float dy) {
 float sample(float x, float y) {
     float sum = 0.0f;
     for(int i=0;i<N;++i) {
-        // float a=TWO_PI*rand()/RAND_MAX;
-        // float a = TWO_PI * i / N;
         float a = TWO_PI * (i + (float)rand()/RAND_MAX)/N;
         sum += trace(x, y, cosf(a), sinf(a));
     }
